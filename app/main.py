7bc78789aef125b68c5b54aeb45cd1398c86b1ff
Ilya Plotnikov <staticneremiah@gmail.com>
2025-04-17 19:26:42 +0300

    Add permission management endpoints and update user model. Refactor main.py - add entry point.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routers import category, products, auth
+import uvicorn
+from app.routers import category, products, auth, permission
 
 app = FastAPI(title='FastAPI e-commerce test application')
 
@@ -11,3 +12,8 @@ async def welcome() -> dict:
 app.include_router(category.router)
 app.include_router(products.router)
 app.include_router(auth.router)
+app.include_router(permission.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
